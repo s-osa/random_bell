2f88f3ef97d829afc6d74acff1eea70d2b21bd9f
OSA Shunsuke <shunsuke-osa@cookpad.com>
2017-11-09 04:36:20 +0900

    Doc

--- lib/random_bell.rb
+++ lib/random_bell.rb
@@ -1,6 +1,10 @@
 require "random_bell/version"
 
 class RandomBell
+  # @param mu     [Float]
+  # @param sigma  [Float]
+  # @param range  [Range]
+  # @param method [Symbol] Method to generate standard normal distribution (:box_muller or :central_limit)
   def initialize(mu: 0.5, sigma: 0.2, range: 0.0..1.0, method: :box_muller)
     @mu     = mu
     @sigma  = sigma
@@ -8,14 +12,17 @@ class RandomBell
     @method = method
   end
 
+  # @return [Float]
   def standard
     send(@method)
   end
 
+  # @return [Float]
   def normal
     @mu + standard * @sigma
   end
 
+  # @return [Float]
   def rand
     loop do
       num = normal
@@ -23,6 +30,7 @@ class RandomBell
     end
   end
 
+  # @return [String]
   def to_histogram(rows: 20, size: 10 ** 4)
     histogram = ""
 
@@ -44,6 +52,7 @@ class RandomBell
 
   private
 
+  # @return [Float]
   def box_muller
     theta  = 2 * Math::PI * Random.rand
     r      = Math.sqrt( -2 * Math.log(Random.rand) )
@@ -51,6 +60,7 @@ class RandomBell
     [z1, z2].sample
   end
 
+  # @return [Float]
   def central_limit
     arr = []
     12.times{ arr << Random.rand }
